f14a1fb5d986e64275727f42a284956f56c884fc
Louis Pioquinto <imalive11@github.com>
2023-12-19 13:48:59 +0800

    Fixed Configuration file and application.properties

--- src/main/java/com/carmada/security/config/SecurityConfig.java
+++ src/main/java/com/carmada/security/config/SecurityConfig.java
@@ -1,100 +1,94 @@
 package com.carmada.security.config;
 
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-@ComponentScan(basePackages="com.carmada")
-public class SecurityConfig {
-
-	// add a reference to our security data source
-	
-	private DataSource securityDataSource;
-
-	@Autowired
-	public SecurityConfig(@Qualifier("securityDataSource")DataSource theSecurityDataSource) {
-		securityDataSource = theSecurityDataSource;
-	}
-
-	@Bean
-	public UserDetailsManager userDetailsManager() {
-		return new JdbcUserDetailsManager(securityDataSource);
-	}
-
-	@Bean
-	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		return http
-			.csrf()
-	        .and()
-			.authorizeRequests(configurer -> 
-				configurer
-					.antMatchers("/*").hasRole("EMPLOYEE")
-					.antMatchers("/drivers/*").hasRole("EMPLOYEE")
-					.antMatchers("/payments/*").hasRole("EMPLOYEE")
-					.antMatchers("/vehicles/*").hasRole("EMPLOYEE")
-					.antMatchers("/accounting/*").hasRole("EMPLOYEE")
-					)
-			
-			.formLogin(configurer -> 
-				configurer
-					.loginPage("/login")
-					.loginProcessingUrl("/authenticateTheUser")
-					.defaultSuccessUrl("/")
-					.permitAll())
-			
-			.logout(configurer -> 
-				configurer
-					.permitAll())
-			
-			.exceptionHandling(configurer -> 
-				configurer
-					.accessDeniedPage("/access-denied"))
-			
-			.build();
-	}
-	
-		
-}
 
 //@Configuration
 //@EnableWebSecurity
-//@ComponentScan(basePackages = "com.carmada")
+//@ComponentScan(basePackages="com.carmada")
 //public class SecurityConfig {
 //
-//	 @Bean
-//	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//	        http.csrf()
-//	            .and()
-//	            .authorizeRequests(authz -> authz
-//					.mvcMatchers("/drivers/**").authenticated()
-//					.mvcMatchers("/payments/**").authenticated()
-//					.mvcMatchers("/vehicles/**").authenticated()
-//					.mvcMatchers("/incidents/**").authenticated()
-//					.mvcMatchers("/accounting/**").authenticated()
-//					.mvcMatchers("/*").authenticated()
+//	// add a reference to our security data source
+//	
+//	private DataSource securityDataSource;
+//
+//	@Autowired
+//	public SecurityConfig(@Qualifier("securityDataSource")DataSource theSecurityDataSource) {
+//		securityDataSource = theSecurityDataSource;
+//	}
+//
+//	@Bean
+//	public UserDetailsManager userDetailsManager() {
+//		return new JdbcUserDetailsManager(securityDataSource);
+//	}
+//
+//	@Bean
+//	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//		return http
+//			.csrf()
+//	        .and()
+//			.authorizeRequests(configurer -> 
+//				configurer
+//					.antMatchers("/*").hasRole("EMPLOYEE")
+//					.antMatchers("/drivers/*").hasRole("EMPLOYEE")
+//					.antMatchers("/payments/*").hasRole("EMPLOYEE")
+//					.antMatchers("/vehicles/*").hasRole("EMPLOYEE")
+//					.antMatchers("/accounting/*").hasRole("EMPLOYEE")
 //					)
-//	            .oauth2Login()
-//	            	.loginPage("/login")
-//	            	.defaultSuccessUrl("/home")
-//	            	.permitAll()
-//	            .and()
-//	            .logout()
-//	            .logoutUrl("/logout")
-//	            .logoutSuccessUrl("/login");
-//	        return http.build();
-//	    }
-//	 
+//			
+//			.formLogin(configurer -> 
+//				configurer
+//					.loginPage("/login")
+//					.loginProcessingUrl("/authenticateTheUser")
+//					.defaultSuccessUrl("/")
+//					.permitAll())
+//			
+//			.logout(configurer -> 
+//				configurer
+//					.permitAll())
+//			
+//			.exceptionHandling(configurer -> 
+//				configurer
+//					.accessDeniedPage("/access-denied"))
+//			
+//			.build();
+//	}
+//	
+//		
 //}
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan(basePackages = "com.carmada")
+public class SecurityConfig {
+
+	 @Bean
+	 public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http.csrf()
+	            .and()
+	            .authorizeRequests(authz -> authz
+					.mvcMatchers("/drivers/**").authenticated()
+					.mvcMatchers("/payments/**").authenticated()
+					.mvcMatchers("/vehicles/**").authenticated()
+					.mvcMatchers("/incidents/**").authenticated()
+					.mvcMatchers("/accounting/**").authenticated()
+					.mvcMatchers("/*").authenticated()
+					)
+	            .oauth2Login()
+	            	.loginPage("/login")
+	            	.defaultSuccessUrl("/home")
+	            	.permitAll()
+	            .and()
+	            .logout()
+	            .logoutUrl("/logout")
+	            .logoutSuccessUrl("/login");
+	        return http.build();
+	    }
+	 
+}
 
 
